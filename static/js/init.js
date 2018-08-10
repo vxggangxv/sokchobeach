08a6736bca581111eb28af1d4fe1ae3dba5a51a7
강혜준 <vxggangxv@naver.com>
2018-08-10 12:22:22 +0900

    메인완료

--- static/js/init.js
+++ static/js/init.js
@@ -1,18 +1,40 @@
 $(function () {
-	$(".contents .slide-container").bxSlider({
+	init();
+	
+	sliderFn();
+	
+});
+
+function init() {
+	var contents = $(".contents"),
+		main = $(".contents .wrap.main"),
+		mainSlide = $(".contents .wrap.main .slide-container"),
+		wh = $(window).height();
+	
+	$(window).resize(function() {
+		wh = $(window).height();
+		main.height(wh);	
+		mainSlide.height(wh);	
+	}).resize();
+}
+
+function sliderFn() {
+	var slider = $(".contents .slide-container");
+	slider.bxSlider({
+//		mode: 'vertical',
 		mode: 'fade',
 		auto: true,
 		autoControls: true,
-		speed: 1000,
-		duration: 6000,
+		speed: 800,
+		duration: 5000,
 		onSliderLoad: function(currentIndex){
-			$(".slide").eq(currentIndex).addClass("active");
-		},
-		onSlideBefore: function ($slideElement, oldIndex, newIndex) {
-			$(".slide").eq(oldIndex).removeClass("active");
+			slider.children().addClass("active");
+			slider.children().eq(currentIndex + 1).removeClass("active");
 		},
 		onSlideAfter: function ($slideElement, oldIndex, newIndex) {
+			slider.children().removeClass("active");
 			$slideElement.addClass("active");
 		}
 	});
-});
+	
+}
